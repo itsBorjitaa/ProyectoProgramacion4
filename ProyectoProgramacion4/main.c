f52befa6779a581404b51dae6a91bc5a1861afc1
Sergio Martínez García <sergio.m@opendeusto.es>
2024-04-02 19:05:46 +0200

    Crear un metodo InicializarBaseDatos

--- ProyectoProgramacion4/main.c
+++ ProyectoProgramacion4/main.c
@@ -29,14 +29,49 @@ void escribirLog(const char *log) {
         printf("Error al abrir el fichero de log.\n");
     }
 }
+void InicializarBD(sqlite3 *db,sqlite3_stmt *stmt){
+	sqlite3_open(DB_FILE, &db);
 
+	//Crear tablas si no existen:
+
+	//Tabla Usuarios
+	char crearUsuario[] ="CREATE TABLE IF NOT EXISTS Usuario (usuario VARCHAR(255) NOT NULL,"
+			"contrasenya VARCHAR(255) NOT NULL,PRIMARY KEY(usuario))";//Statement crear usuario
+	sqlite3_prepare_v2(db, crearUsuario, strlen(crearUsuario) + 1, &stmt, NULL);//Cargamos el stmt
+	sqlite3_step(stmt);//Ejecutamos el statement
+
+	//Tabla Gastos
+	char crearGasto[]="CREATE TABLE IF NOT EXISTS Gastos "
+			"(codigo INTEGER NOT NULL DEFAULT 0, usuarioF TEXT NOT NULL, "
+			"fecha TEXT NOT NULL, concepto TEXT NOT NULL, "
+			"coste REAL NOT NULL, categoria TEXT NOT NULL,PRIMARY KEY(codigo), "
+			"FOREIGN KEY(usuarioF) REFERENCES Usuario(usuario) ON DELETE CASCADE)";
+	sqlite3_prepare_v2(db, crearGasto, strlen(crearGasto) + 1, &stmt, NULL);
+	sqlite3_step(stmt);
+
+	//Tabla Categorias
+	char crearCategoria[]="CREATE TABLE IF NOT EXISTS Categorias (id_c INTEGER NOT NULL, "
+			"categoria STRING NOT NULL, PRIMARY KEY(id_c))";
+	sqlite3_prepare_v2(db, crearCategoria, strlen(crearCategoria) + 1, &stmt, NULL);
+	sqlite3_step(stmt);
+
+	//Tabla CategoriasUsuario
+	char crearCategoriasUsuario[]="CREATE TABLE IF NOT EXISTS categoriasUsuario (usuario_cu String NOT NULL, "
+			"id_c_cu INTEGER NOT NULL,PRIMARY KEY(usuario_cu, id_c_cu), "
+			"FOREIGN KEY(usuario_cu) REFERENCES Usuario(usuario) "
+			"ON DELETE CASCADE, FOREIGN KEY(id_c_cu) REFERENCES  Categorias(id_c) ON DELETE CASCADE)";
+	sqlite3_prepare_v2(db, crearCategoriasUsuario, strlen(crearCategoriasUsuario) + 1, &stmt, NULL);
+	sqlite3_step(stmt);
+}
 
 int main(){
+	sqlite3 *db;//
+	sqlite3_stmt *stmt;
 	ListaUsuarios lu;
 	Usuario u;
 	int pos;
 	char opcion,opcionU;
-
+	InicializarBD(db,stmt);
 	volcarFicheroAListaUsuarios(&lu, NOMFICH);
 
 	escribirLog("Prueba de inicio");
