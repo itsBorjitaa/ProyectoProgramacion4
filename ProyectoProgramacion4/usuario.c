99571f48e00cb740630dfc84f8e4b190b740282c
Eneko Saez <eneko.saez@opendeusto.es>
2024-04-03 12:39:34 +0200

    Añadidas funciones de usuarios con BD

--- ProyectoProgramacion4/usuario.c
+++ ProyectoProgramacion4/usuario.c
@@ -1,6 +1,7 @@
 #include "usuario.h"
 #include <string.h>
 #include <stdio.h>
+#include "sqlite3.h"
 
 Usuario pedirUsuario(){
 	Usuario u;
@@ -23,6 +24,34 @@ int contrasenyaCorrecta(char *conU,char *con){
 		correcta = 1;
 	}
 	return correcta;
+}
+
+void insertarUsuario(Usuario usuario, sqlite3 *db,sqlite3_stmt *stmt) {
+	Usuario nuevoUsuario;
+
+	//Codigo para ejecutar el statement
+	char insertarUsuario[] = "INSERT INTO Usuarios VALUES(?, ?)";
+	sqlite3_prepare_v2(db, insertarUsuario, sizeof(insertarUsuario) + 1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, nuevoUsuario.nombre, sizeof(nuevoUsuario.nombre), SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, nuevoUsuario.contrasenya, sizeof(nuevoUsuario.contrasenya), SQLITE_STATIC);
+	sqlite3_step(stmt);
+}
+
+Usuario buscarUsuario(char nombre, sqlite3 *db,sqlite3_stmt *stmt) {
+	Usuario usuario;
+
+	//Codigo para ejecutar los statements
+	char buscarUsuario[] = "SELECT usuario FROM Usuario WHERE usuario = ?";
+	sqlite3_prepare_v2(db, buscarUsuario, sizeof(buscarUsuario) + 1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, nombre, sizeof(nombre), SQLITE_STATIC);
+	usuario.nombre = sqlite3_step(stmt);
+
+	char buscarContra[] = "SELECT contrasenya FROM Usuario WHERE usuario = ?";
+	sqlite3_prepare_v2(db, buscarContra, sizeof(buscarContra) + 1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, nombre, sizeof(nombre), SQLITE_STATIC);
+	usuario.contrasenya = sqlite3_step(stmt);
+
+	return usuario;
 }
 
 
